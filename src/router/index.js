b2ad08ac5d7eee3dfffc53edd324f3859eb2b154
Abbygaile Blue Calitis <calitisabby@gmail.com>
2024-04-18 02:14:23 +0800

    update routers for addproduct

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import ProductList from '../components/ProductList.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import ProductList from "../components/ProductList.vue";
+import addProduct from "../components/AddProducts.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: ProductList
+    path: "/",
+    name: "home",
+    component: ProductList,
   },
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  // }
-]
+  {
+    path: "/addproduct",
+    name: "addproduct",
+    component: addProduct,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
